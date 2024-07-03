f2405b358842674d355318a2e15576cc24da70a7
Munira <moonbrig@gmail.com>
2024-07-03 15:32:52 +0800

    remove unused imports

--- src/test/java/com/lautbiru/productRegistration/controller/ProductServiceControllerTest.java
+++ src/test/java/com/lautbiru/productRegistration/controller/ProductServiceControllerTest.java
@@ -7,19 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.lautbiru.productRegistration.model.Product;
 import com.lautbiru.productRegistration.service.ProductServiceImpl;
 
-import static org.hamcrest.Matchers.aMapWithSize;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalToObject;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
